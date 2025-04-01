22520dbb276965d774ac1d21f7e580f05ca75fc2
archeana <anaclaudiasousajesus6@gmail.com>
2025-04-01 12:49:58 +0200

    feat:  link to detail page

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,29 @@
+"use client";
 
 import { Container, Typography } from "@mui/material";
 import ChartCard from "./components/ChartCard";
 import { chartData } from "./data/charts";
 import Grid from "@mui/material/Grid";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       <Container sx={{ py: 4 }}>
         <Typography variant="h4" gutterBottom>
-          30 days Chart Challenge
+          Chart Gallery
         </Typography>
         <Grid container spacing={3}>
           {chartData.map((chart) => (
             <Grid key={chart.id}>
-              <ChartCard
-                {...chart}
-                description={chart.description || "Default description"}
-                chartImage={chart.image || "default-image-url"}
-              />
+              {/* Wrap the chart card with a Link to the chart detail page */}
+              <Link href={`/charts/${chart.id}`} passHref>
+                <ChartCard
+                  {...chart}
+                  description={chart.description || "Default description"}
+                  chartImage={chart.image || "default-image-url"}
+                />
+              </Link>
             </Grid>
           ))}
         </Grid>
